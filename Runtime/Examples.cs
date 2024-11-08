0bb1df2e10c26926199015ca652ac934b88cc01d
Glurth <Glurth@gmail.com>
2024-11-08 15:04:59 -0700

    moved samples to own file, made unity classes part of main namepsace

---
+++ Runtime/Examples.cs
@@ -0,0 +1,207 @@
+using UnityEngine.Events;
+using System.Collections.Generic;
+using EyE.StateMachine;
+namespace EyE.StateMachine.Samples
+{
+    //sample gameState class- contains data about the state of the game, and has function to load/save/start new
+    public class GameStateData
+    {
+        public bool gameStarted;
+        public async void InitForNewGame()
+        {
+            //await someProcessing()
+            gameStarted = true;
+        }
+        public async void SaveToFile(string filname)
+        {
+            //await file write stuff...
+        }
+
+        public static async System.Threading.Tasks.Task<GameStateData> LoadFromFile(string filname, UnityEvent onComplete)
+        {
+            GameStateData loadedData = null;
+            //stuff..
+            //await someProcess()
+            onComplete.Invoke();
+            return loadedData;
+        }
+        //other stuff...
+    }
+
+    public class MenuState : UnityEventDrivenState
+    {
+        GameStateData gameData;
+        SceneUIObjectReferences sceneObjects;
+
+        public MenuState(GameStateData gameData, SceneUIObjectReferences sceneObjects)
+        {
+            this.gameData = gameData;
+            this.sceneObjects = sceneObjects;
+
+        }
+        protected override List<UnityEventSubscription> GetSubscribers()
+        {
+            return new List<UnityEventSubscription>()
+            {
+                new UnityEventSubscription(sceneObjects.menuWindow.newGame.onClick,HandleNewGameClick),
+                new UnityEventSubscription(sceneObjects.menuWindow.saveGame.onClick,HandleSaveGameClick),
+                new UnityEventSubscription(sceneObjects.menuWindow.quitGame.onClick,HandleQuitGameClick),
+            };
+        }
+        protected override void HandleActivateState()
+        {
+            sceneObjects.menuWindow.gameObject.SetActive(true);
+            sceneObjects.menuWindow.saveGame.interactable = gameData.gameStarted;
+        }
+        protected override void HandleDeActivateState()
+        {
+            sceneObjects.menuWindow.gameObject.SetActive(false);
+        }
+        void HandleNewGameClick()
+        {
+            UnityEvent initCompleteTrigger = new UnityEvent();
+            ChangeState(new WaitScreenForProcess(sceneObjects.waitDisplay, this, initCompleteTrigger));
+            System.Threading.Tasks.Task.Run(gameData.InitForNewGame).ContinueWith((o) => { initCompleteTrigger.Invoke(); });
+            //run task will not be awaited.  instead the initCompleteTrigger will be invoked when done.
+        }
+        void HandleSaveGameClick()
+        {
+            ChangeState(new SaveGameMenuState(gameData, sceneObjects));
+        }
+        void HandleQuitGameClick()
+        {
+            UnityEngine.Application.Quit();
+        }
+    }
+
+    public class SaveGameMenuState : UnityEventDrivenState
+    {
+        SceneUIObjectReferences sceneObjects;
+        GameStateData gameData;
+
+        public SaveGameMenuState(GameStateData gameData, SceneUIObjectReferences sceneObjects)
+        {
+            this.gameData = gameData;
+            this.sceneObjects = sceneObjects;
+
+        }
+        protected override List<UnityEventSubscription> GetSubscribers()
+        {
+            return new List<UnityEventSubscription>()
+            {
+                new UnityEventSubscription(sceneObjects.fileNameWindow.fileNameSelectedEvent,HandleSaveFile),//example using an event/handler that passes/takes a data object.
+                new UnityEventSubscription(sceneObjects.fileNameWindow.cancelEvent,Cancel),
+            };
+        }
+        protected override void HandleActivateState()
+        {
+            sceneObjects.fileNameWindow.gameObject.SetActive(true);
+        }
+        protected override void HandleDeActivateState()
+        {
+            sceneObjects.fileNameWindow.gameObject.SetActive(false);
+        }
+        void Cancel()
+        {
+            ChangeState(new MenuState(gameData, sceneObjects)); //return to the main menu state
+        }
+        void HandleSaveFile(object filename)
+        {
+            UnityEvent saveCompleteCallback = new UnityEvent();// this will be passed to the new WaitScreenForProcess state (which will listen to it), and to the SaveToFile function (which will trigger it when done).
+            ChangeState(new WaitScreenForProcess(sceneObjects.waitDisplay, new MenuState(gameData, sceneObjects), saveCompleteCallback));
+            System.Threading.Tasks.Task
+                .Run(() => { gameData.SaveToFile((string)filename); })
+                .ContinueWith((o) => { saveCompleteCallback.Invoke(); });
+
+        }
+    }
+
+    public class WaitScreenForProcess : UnityRevertibleEventDrivenState
+    {
+
+        WaitThingy waitDisplay;
+        UnityEvent processIsCompleteTrigger;
+        public WaitScreenForProcess(WaitThingy waitDisplay, UnityEventDrivenState stateToChangeToWhenComplete, UnityEvent processIsCompleteTrigger) : base(stateToChangeToWhenComplete)
+        {
+            this.waitDisplay = waitDisplay;
+            this.processIsCompleteTrigger = processIsCompleteTrigger;
+        }
+        protected override List<UnityEventSubscription> GetSubscribers()
+        {
+            return new List<UnityEventSubscription>()
+            {
+                 new UnityEventSubscription(processIsCompleteTrigger,()=>{Revert();}),
+            };
+        }
+        protected override void HandleActivateState()
+        {
+            waitDisplay.gameObject.SetActive(true);
+        }
+        protected override void HandleDeActivateState()
+        {
+            waitDisplay.gameObject.SetActive(false);
+        }
+
+    }
+
+    public class UserConfirm : UnityEventDrivenState
+    {
+        YesNoWindow yesnoDisplay;
+        UnityAction yesHandler;
+        UnityAction noHandler;
+        public UserConfirm(YesNoWindow yesnoDisplay, UnityAction yesHandler, UnityAction noHandler)
+        {
+            this.yesnoDisplay = yesnoDisplay;
+            this.yesHandler = yesHandler;
+            this.noHandler = noHandler;
+        }
+        protected override List<UnityEventSubscription> GetSubscribers()
+        {
+            return new List<UnityEventSubscription>()
+            {
+                new UnityEventSubscription(yesnoDisplay.yesEvent,yesHandler),
+                new UnityEventSubscription(yesnoDisplay.noEvent, noHandler),
+            };
+        }
+        protected override void HandleActivateState()
+        {
+            yesnoDisplay.gameObject.SetActive(true);
+        }
+        protected override void HandleDeActivateState()
+        {
+            yesnoDisplay.gameObject.SetActive(false);
+        }
+    }
+
+    // below are example UI component classes that are used by the example states above
+    public class SelectFileNameWindow : UnityEngine.MonoBehaviour
+    {
+        public UnityEvent<object> fileNameSelectedEvent;
+        public UnityEvent cancelEvent;
+        //UI stuff...
+    }
+    public class WaitThingy : UnityEngine.MonoBehaviour
+    {
+        //UI stuff..
+    }
+    public class YesNoWindow : UnityEngine.MonoBehaviour
+    {
+        public UnityEvent yesEvent;
+        public UnityEvent noEvent;
+        //UI stuff..
+    }
+    public class MainMenuWindow : UnityEngine.MonoBehaviour
+    {
+        public UnityEngine.UI.Button newGame;
+        public UnityEngine.UI.Button saveGame;
+        public UnityEngine.UI.Button quitGame;
+    }
+
+    //object that will hold references to all the various UI components needed by the state machine
+    public class SceneUIObjectReferences : UnityEngine.MonoBehaviour
+    {
+        public MainMenuWindow menuWindow;
+        public SelectFileNameWindow fileNameWindow;
+        public WaitThingy waitDisplay;
+    }
+}
--- Runtime/VariableEventDrivenStateBase.cs
+++ Runtime/VariableEventDrivenStateBase.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 
 namespace EyE.StateMachine
 {
-    ///////////////////////////////////////////////////////////////
-    ///  OPTION 1
-    ////////////////////////////////////////////////////////////
 
-
-    /// <summary>
-    /// The `VariableEventDrivenState` class and its descendants are used to represent different states in a system.
-    /// All state classes inherit from `VariableEventDrivenState<TEventProviderType>`, providing a simple event-driven and polymorphic approach to state management.
+    /// The `EventDrivenState` class and its descendants are used to represent different states in a system.
+    /// All state classes inherit from `EventDrivenState<TEventProviderType>`, providing a simple event-driven and polymorphic approach to state management.
     /// State changes occur in response to events, which will internally call the two abstract functions (subscribe/unsubscribe) defined by the descendant/user of this class.
     ///
     /// The source of the "driving" events, usually a scene object, must be passed to the constructor of every state.
     /// Each state class defined by the user specifies the events it listens to, and how it reacts to each one.
     /// State transitions are accomplished by calling the `ChangeState` method, and passing in a new instance of the desired state class (which will, in turn, require an instance of its TEventProviderType).
-    /// The current state is defined by the events to which the state is subscribed, rather than being explicitly stored- this is all ChangeState really does.
+    /// The current state is defined by the events to which the state is subscribed, rather than being explicitly stored- this is all changing state really does.
     /// State changes are controlled within each state class in response to events, as defined by the user, allowing for dynamic transitions based on events.
     /// The subscribe/unsubscribe functions can implement polymorphic behavior, but the hierarchy must use the same TEventProviderType to do this. 
     /// 
@@ -32,110 +27,13 @@ namespace EyE.StateMachine
     /// These functions handle the actions triggered by events, which may include changing the state.
     /// Users may choose to modify scene/state values inside this class when events are triggered or implement `IHandleStateChangeEvents` on the `eventSource`.
     /// The purpose of IHandleStateChangeEvents is to allow the event source object to react when the state changes.
-    /// </summary>
 
-    // Define the internal Subscribe interface- this interface exists so that VariableEventDrivenStateBase can define SubscribeToEvents as protected, and still allow it to be invokable from inside VariableEventDrivenState<TEventProviderType>
-    internal interface ISubscribeToEvents
-    {
-        void Subscribe();
-    }
-
-
-    /// <summary>
-    /// Event source objects may, optionally, choose to implement this interface should they wish to be notified of state changes.
-    /// </summary>
-    public interface IHandleStateChangeEvents
-    {
-        //        public void HandleUIStateChanged(VariableEventDrivenStateBase newState);
-        public void HandleUIStateChanged<TStateType>(TStateType newState) where TStateType : VariableEventDrivenStateBase;
-
-    }
-
-    /// <summary>
-    /// The base class for variable event-driven states, implementing the `ISubscribeToEvents` interface.
-    /// Descendants must override `SubscribeToEvents` and `UnSubscribeToEvents` functions.
-    /// </summary>
-    abstract public class VariableEventDrivenStateBase : ISubscribeToEvents
-    {
-        // Explicitly implement the internal interface
-        void ISubscribeToEvents.Subscribe()
-        {
-            SubscribeToEvents();
-        }
-
-        /// <summary>
-        /// Override this function to subscribe to events when the state is entered.
-        /// </summary>
-        abstract protected void SubscribeToEvents();
-
-        /// <summary>
-        /// Override this function to unsubscribe from events when the state is exited.
-        /// </summary>
-        abstract protected void UnSubscribeToEvents();
-
-
-    }
-
-    /// <summary>
-    /// A generic implementation of a variable event-driven state.
-    /// Expands upon the VariableEventDrivenStateBase by adding a member that specifies the source of events that will drive this state.
-    /// Includes a ChangeState function.  This function will unsubscribe this state from vents and subscribe the new state.  Also, if the eventSource implements  IHandleStateChangeEvents it will notify it of the state change.
-    /// Provide a static function for creating a root state instance.
-    /// </summary>
-    /// <typeparam name="TEventProviderType">The type of the event provider/source.</typeparam>
-    abstract public class VariableEventDrivenState<TEventProviderType> : VariableEventDrivenStateBase
-    {
-        /// <summary>
-        /// The event provider/source for this state.
-        /// </summary>
-        protected TEventProviderType eventSource { get; private set; }
-
-        /// <summary>
-        /// Initializes a new instance of the `VariableEventDrivenState` class with the specified event source.
-        /// Does NOT subscribe to events in here.  This is only done when the state is changed, or when the static CreasteRootState function is invoked.
-        /// </summary>
-        /// <param name="eventSource">The event provider/source for this state.</param>
-        public VariableEventDrivenState(TEventProviderType eventSource)
-        {
-            this.eventSource = eventSource;
-        }
-        private VariableEventDrivenState() { }
-        public static TStateType CreateRootState<TStateType>(TEventProviderType eventSource) where TStateType : VariableEventDrivenState<TEventProviderType>, new()
-        {
-            TStateType newRoot = new TStateType();
-            newRoot.eventSource = eventSource;
-            newRoot.SubscribeToEvents();
-            return newRoot;
-        }
-
-        /// <summary>
-        /// Changes the current state to a new state.
-        /// </summary>
-        /// <param name="newState">The new state to transition to.</param>
-        public void ChangeState(VariableEventDrivenStateBase newState)
-        {
-            UnSubscribeToEvents();
-
-            if (newState is ISubscribeToEvents subscribeable) //we know it is
-                subscribeable.Subscribe();
-
-            if (eventSource is IHandleStateChangeEvents eventMonitor)
-                eventMonitor.HandleUIStateChanged(newState);
-        }
-    }
-
-    ///////////////////////////////////////////////////////////////
-    ///  OPTION 2
-    ////////////////////////////////////////////////////////////
-    ////rather than the user defining the subscribe/unsubscribe functions with a bunch of lines that all say ``event.AddListener(handler)``, a list of pair/handlers is defined by the user, and adding/remving listeners to subscribe/unsubscibe is done automatically.
 
     public interface ISubscriber
     {
         public void Subscribe();
         public void Unsubscribe();
     }
-
-
 
     /// <summary>
     /// This abstract class handles subscribing and unsubscribing to a list of Events(or whatever), each with a particular UnityAction handler.
@@ -172,7 +70,7 @@ namespace EyE.StateMachine
 
     /// <summary>
     /// Derive from this class to create a "State" in the state machine.  All the different, derived variants define the different states of the statemachine.
-    /// Override the eventsAndHandlers get accessor to define what events to listen to, and what action to take when they are triggered, for a given state.
+    /// Override the GetSubscribers method to define what events to listen to, and what action to take when they are triggered, for a given state.
     /// It is expected that derived variants will define a constructor with a(some) parameter(s) that will provide access to scene objects that will generate the events it needs to subscribe to.
     /// When a given state is activated, it's events are subscribed to, while the now-previous state's events are unsubscribed from.
     /// The current state is defined by which events are currently subscribed to, it is not actually stored anywhere specific- though the user may choose to do this themselves.
@@ -198,7 +96,7 @@ namespace EyE.StateMachine
             newState.ActivateLayerStates();
         }
         #region layeredStates
-        private HashSet<EventDrivenStateLayer<TEventSubscriber>> layeredStates = new HashSet<EventDrivenStateLayer<TEventSubscriber>>();
+        private List<EventDrivenStateLayer<TEventSubscriber>> layeredStates = new List<EventDrivenStateLayer<TEventSubscriber>>();
         //call this function to add activate an additional state(set of event listeners) to this one
         // does NOT deactivate this state
         protected void LayerNewState(EventDrivenStateLayer<TEventSubscriber> newState)
@@ -229,6 +127,7 @@ namespace EyE.StateMachine
 
     //Can be activated via LayerNewState, which will NOT deactivate the calling state, like ChangeState would.
     //These states can be deactivated without having to initiate a new state.
+    //These states will be activated and deactivated when the state they are layered upon is
     abstract public class EventDrivenStateLayer<TEventSubscriber> : EventDrivenState<TEventSubscriber> where TEventSubscriber : ISubscriber
     {
         public void Terminate()
@@ -237,6 +136,7 @@ namespace EyE.StateMachine
             UnSubscribeFromAll();
         }
     }
+    
     //has special constructor.  stores ref to the state that will be changed to when Revert() is called.
     //Can used like a stack by passing "this" to constuctor.
     abstract public class RevertibleEventDrivenState<TEventSubscriber> : EventDrivenState<TEventSubscriber> where TEventSubscriber : ISubscriber
@@ -252,7 +152,6 @@ namespace EyE.StateMachine
             ChangeState(stateToRevertTo);
         }
     }
-
 
     //examples
     //class used to store an Event and a function to invoke when the event is triggered
@@ -292,6 +191,7 @@ namespace EyE.StateMachine
     }
 
     abstract public class UnityEventDrivenState : EventDrivenState<UnityEventSubscription> { }
+    abstract public class UnityEventDrivenStateLayer : EventDrivenStateLayer<UnityEventSubscription> { }
     abstract public class UnityRevertibleEventDrivenState : RevertibleEventDrivenState<UnityEventSubscription>
     {
         protected UnityRevertibleEventDrivenState(EventDrivenState<UnityEventSubscription> stateToRevertTo) : base(stateToRevertTo)
@@ -300,207 +200,7 @@ namespace EyE.StateMachine
     }
 
 
-    //sample gameState class- contains data about the state of the game, and has function to load/save/start new
-    public class GameStateData
-    {
-        public bool gameStarted;
-        public async void InitForNewGame()
-        {
-            gameStarted = true;
-        }
-        public async void SaveToFile(string filname)
-        {
-            //stuff...
 
-        }
-
-        public static async System.Threading.Tasks.Task<GameStateData> LoadFromFile(string filname, UnityEvent onComplete)
-        {
-            //stuff..
-            onComplete.Invoke();
-            return new GameStateData(); // <= bad example
-        }
-        //other stuff...
-    }
 
 }
-namespace EyE.StateMachine.Samples
-{
-    //NON-stack example
-    public class MenuState : UnityEventDrivenState
-    {
-        GameStateData gameData;
-        SceneUIObjects sceneObjects;
 
-        public MenuState(GameStateData gameData, SceneUIObjects sceneObjects)
-        {
-            this.gameData = gameData;
-            this.sceneObjects = sceneObjects;
-
-        }
-        protected override List<UnityEventSubscription> GetSubscribers()
-        {
-            return new List<UnityEventSubscription>()
-            {
-                new UnityEventSubscription(sceneObjects.menuWindow.newGame.onClick,HandleNewGameClick),
-                new UnityEventSubscription(sceneObjects.menuWindow.saveGame.onClick,HandleSaveGameClick),
-                new UnityEventSubscription(sceneObjects.menuWindow.quitGame.onClick,HandleQuitGameClick),
-            };
-        }
-        protected override void HandleActivateState() 
-        {
-            sceneObjects.menuWindow.gameObject.SetActive(true);
-            sceneObjects.menuWindow.saveGame.interactable = gameData.gameStarted;
-        }
-        protected override void HandleDeActivateState() 
-        {
-            sceneObjects.menuWindow.gameObject.SetActive(false);
-        }
-        void HandleNewGameClick()
-        {
-            UnityEvent initCompleteTrigger = new UnityEvent();
-            ChangeState(new WaitScreenForProcess(sceneObjects.waitDisplay,this, initCompleteTrigger));
-            System.Threading.Tasks.Task.Run(gameData.InitForNewGame).ContinueWith((o)=> { initCompleteTrigger.Invoke(); });
-            //will not be awaited.  instead the initCompleteTrigger will be invoked when done.
-        }
-        void HandleSaveGameClick()
-        {
-            ChangeState(new SaveGameMenuState(gameData, sceneObjects));
-        }
-        void HandleQuitGameClick()
-        {
-            UnityEngine.Application.Quit();
-        }
-    }
-
-    public class SaveGameMenuState : UnityEventDrivenState
-    {
-        SceneUIObjects sceneObjects;
-        GameStateData gameData;
-        
-        public SaveGameMenuState(GameStateData gameData, SceneUIObjects sceneObjects)
-        {
-            this.gameData = gameData;
-            this.sceneObjects = sceneObjects;
-
-        }
-        protected override List<UnityEventSubscription> GetSubscribers()
-        {
-            return new List<UnityEventSubscription>()
-            {
-                new UnityEventSubscription(sceneObjects.fileNameWindow.fileNameSelectedEvent,HandleSaveFile),
-                new UnityEventSubscription(sceneObjects.fileNameWindow.cancelEvent,Cancel),
-            };
-        }
-        protected override void HandleActivateState()
-        {
-            sceneObjects.fileNameWindow.gameObject.SetActive(true);
-        }
-        protected override void HandleDeActivateState()
-        {
-            sceneObjects.fileNameWindow.gameObject.SetActive(false);
-        }
-        void Cancel()
-        {
-            ChangeState(new MenuState(gameData, sceneObjects)); //return to the main menu state
-        }
-        void HandleSaveFile(object filename)
-        {
-            UnityEvent saveCompleteCallback= new UnityEvent();// this will be passed to the new WaitScreenForProcess state (which will listen to it), and to the SaveToFile function (which will trigger it when done).
-            ChangeState(new WaitScreenForProcess(sceneObjects.waitDisplay, new MenuState(gameData, sceneObjects), saveCompleteCallback));
-            System.Threading.Tasks.Task
-                .Run(()=> { gameData.SaveToFile((string)filename); })
-                .ContinueWith((o) => { saveCompleteCallback.Invoke(); });
-
-        }
-    }
-
-    public class WaitScreenForProcess : UnityRevertibleEventDrivenState
-    {
-
-        WaitThingy waitDisplay;
-        UnityEvent processIsCompleteTrigger;
-        public WaitScreenForProcess(WaitThingy waitDisplay, UnityEventDrivenState stateToChangeToWhenComplete, UnityEvent processIsCompleteTrigger):base(stateToChangeToWhenComplete)
-        {
-            this.waitDisplay = waitDisplay;
-            this.processIsCompleteTrigger = processIsCompleteTrigger;
-        }
-        protected override List<UnityEventSubscription> GetSubscribers()
-        {
-            return new List<UnityEventSubscription>()
-            {
-                 new UnityEventSubscription(processIsCompleteTrigger,()=>{Revert();}),
-            };
-        }
-        protected override void HandleActivateState()
-        {
-            waitDisplay.gameObject.SetActive(true);
-        }
-        protected override void HandleDeActivateState()
-        {
-            waitDisplay.gameObject.SetActive(false);
-        }
-
-    }
-
-    public class UserConfirm : UnityEventDrivenState
-    {
-        YesNoWindow yesnoDisplay;
-        UnityAction yesHandler;
-        UnityAction noHandler;
-        public UserConfirm(YesNoWindow yesnoDisplay, UnityAction yesHandler, UnityAction noHandler)
-        {
-            this.yesnoDisplay = yesnoDisplay;
-            this.yesHandler = yesHandler;
-            this.noHandler = noHandler;
-        }
-        protected override List<UnityEventSubscription> GetSubscribers()
-        {
-            return new List<UnityEventSubscription>()
-            {
-                new UnityEventSubscription(yesnoDisplay.yesEvent,yesHandler),
-                new UnityEventSubscription(yesnoDisplay.noEvent, noHandler),
-            };
-        }
-        protected override void HandleActivateState()
-        {
-            yesnoDisplay.gameObject.SetActive(true);
-        }
-        protected override void HandleDeActivateState()
-        {
-            yesnoDisplay.gameObject.SetActive(false);
-        }
-    }
-
-    // below are example UI component classes that are used by the example states above
-    public class SelectFileNameWindow : UnityEngine.MonoBehaviour
-    {
-        public UnityEvent<object> fileNameSelectedEvent;
-        public UnityEvent cancelEvent;
-        //UI stuff...
-    }
-    public class WaitThingy : UnityEngine.MonoBehaviour
-    {
-        //UI stuff..
-    }
-    public class YesNoWindow : UnityEngine.MonoBehaviour
-    {
-        public UnityEvent yesEvent;
-        public UnityEvent noEvent;
-        //UI stuff..
-    }
-    public class MainMenuWindow : UnityEngine.MonoBehaviour
-    {
-        public UnityEngine.UI.Button newGame;
-        public UnityEngine.UI.Button saveGame;
-        public UnityEngine.UI.Button quitGame;
-    }
-
-    //object that will hold references to all the various UI components needed by the state machine
-    public class SceneUIObjects : UnityEngine.MonoBehaviour
-    {
-        public MainMenuWindow menuWindow;
-        public SelectFileNameWindow fileNameWindow;
-        public WaitThingy waitDisplay;
-    }
-}
